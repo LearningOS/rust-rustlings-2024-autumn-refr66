e51246c84d52bbcec358550b72c534767464e4e7
refr66 <cyswsy6661@outlook.com>
2024-10-06 12:22:10 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -1,3 +1,4 @@
+
 // tests9.rs
 //
 // Rust is highly capable of sharing FFI interfaces with C/C++ and other statically compiled
@@ -27,16 +28,16 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
-
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    pub extern "C" fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
